2910fd665b20c35fc0308e59b91f0ca585974237
SystemSolution21 <systemsolution21@gmail.com>
2026-02-19 18:50:00 +0900

    Impemented user invitation information by email.

--- core/settings.py
+++ core/settings.py
@@ -293,9 +293,9 @@ EMAIL_BACKEND = os.getenv(
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_USE_TLS = str(os.getenv("EMAIL_USE_TLS", "True")).lower() in ("true", "1", "t")
-EMAIL_HOST_USER = os.getenv("EMAIL_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASS")
-DEFAULT_FROM_EMAIL = os.getenv("EMAIL_USER")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"  # for development
 

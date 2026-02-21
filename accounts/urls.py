5dfa31cf597b2002e5340420f6227858a0f2a846
SystemSolution21 <systemsolution21@gmail.com>
2026-02-21 14:02:42 +0900

    Enhanced user password rest feature.

--- accounts/urls.py
+++ accounts/urls.py
@@ -17,4 +17,33 @@ urlpatterns: list[URLPattern] = [
     path(route="logout/", view=auth_views.LogoutView.as_view(), name="logout"),
     path(route="signup/", view=SignUpView.as_view(), name="signup"),
     path(route="onboarding/", view=OnboardingView.as_view(), name="onboarding"),
+    path(
+        route="password_reset/",
+        view=auth_views.PasswordResetView.as_view(
+            template_name="registration/password_reset_form.html",
+            email_template_name="registration/password_reset_email.html",
+        ),
+        name="password_reset",
+    ),
+    path(
+        route="password_reset/done/",
+        view=auth_views.PasswordResetDoneView.as_view(
+            template_name="registration/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        route="reset/<uidb64>/<token>/",
+        view=auth_views.PasswordResetConfirmView.as_view(
+            template_name="registration/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        route="reset/done/",
+        view=auth_views.PasswordResetCompleteView.as_view(
+            template_name="registration/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
